abbb57d92061b71da70813a6e03a80136494c34d
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-11 14:13:07 -0400

    Adding Home component and the route.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import {
+        BrowserRouter as Router,
+        Route,
+        Switch
+        } from 'react-router-dom';
+
+console.log("App is working.");
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="app-container">
+          <Switch>
+            <Route path="/weather-forecast/" component={() => (<Home />) }/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ front-end/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+console.log("Home is working.");
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container"> </div>
+    );
+  }
+}
+
+export default Home;
