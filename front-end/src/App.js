45d4d44d75f1414230832e8f6cb736e3d51193aa
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-11 15:19:00 -0400

    Getting API working on console.log

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,10 +6,28 @@ import {
         Route,
         Switch
         } from 'react-router-dom';
+import axios from 'axios';
 
 console.log("App is working.");
 
 class App extends Component {
+
+  componentWillMount() {
+    var call = 'http://api.openweathermap.org/data/2.5/group?id=';
+    var citiesId = '5128581,3451190,3675707,5601538,292223';
+    var units = '&units=imperial';
+    const apiKey = '&appid=c2a8f705fd5c4cdcab53ed003fbf3927';
+    const url = call + citiesId + units + apiKey;
+
+    axios.get(url)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+   }
+
   render() {
     return (
       <Router>
