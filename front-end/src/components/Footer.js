4bd0bdcc03e77684660594397fa137289759843f
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-12 13:29:04 -0400

    Adding footer.

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+console.log("Footer is working.");
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>  
+        <p class="footer">Handmade by <a class="footer" href="https://github.com/francheska-guzman" rel="noopener noreferrer" target="_blank">Francheska Guzman</a>.</p>
+      </footer> 
+    );
+  }
+}
+
+export default Footer;
