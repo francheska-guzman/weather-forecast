4dd26dd70668c777b2bd495c46a2f0f787824d08
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2019-03-23 00:32:06 -0400

    Updates

---
+++ precache-manifest.c56c6de77ddfde259a3745da709eae5f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  },
+  {
+    "revision": "530caccb6cfceb47573b",
+    "url": "/weather-forecast/static/js/main.e02ab0f8.chunk.js"
+  },
+  {
+    "revision": "bb171f62cc94bfa788d5",
+    "url": "/weather-forecast/static/js/2.8917574f.chunk.js"
+  },
+  {
+    "revision": "530caccb6cfceb47573b",
+    "url": "/weather-forecast/static/css/main.7803064c.chunk.css"
+  },
+  {
+    "revision": "2814ffe962a6591d9a226cf18920fed2",
+    "url": "/weather-forecast/index.html"
+  }
+];
