5879eceb3b608d1745278480de8d812ac61936b9
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2019-09-19 17:51:09 -0400

    Updates

---
+++ precache-manifest.71cb2e4a0a4266e25bf98c770263fdb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf231801beb689cac3f841933427ac80",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "4ddd121b07c2cc3955b0",
+    "url": "/weather-forecast/static/css/main.8fc3c575.chunk.css"
+  },
+  {
+    "revision": "db93dc3c8f1755112766",
+    "url": "/weather-forecast/static/js/2.152033b4.chunk.js"
+  },
+  {
+    "revision": "4ddd121b07c2cc3955b0",
+    "url": "/weather-forecast/static/js/main.7f204228.chunk.js"
+  },
+  {
+    "revision": "c25bbf701e8a364cae6e",
+    "url": "/weather-forecast/static/js/runtime-main.b41c5f49.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-forecast/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-forecast/precache-manifest.b029f4c9f94d3b876e14ca1a64923d48.js"
+  "/weather-forecast/precache-manifest.71cb2e4a0a4266e25bf98c770263fdb1.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/weather-forecast/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-forecast/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
